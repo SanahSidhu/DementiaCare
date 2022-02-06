ca2285a1313c99f591546ebb7eaa72590c4bfe4b
rajakiru <raja.kiruthika@yahoo.com>
2022-02-06 17:44:57 +0900

    appstack

---
+++ client/appstack 17.41.43/UserScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function UserScreen() {
+    return (
+        <View>
+            <Text> This is the User Screen </Text>
+        </View>
+    );
+}
