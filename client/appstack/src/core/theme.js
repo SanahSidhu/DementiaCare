ad9b9fc4288887fa52a04b1eabcc57c36ca69262
rajakiru <raja.kiruthika@yahoo.com>
2022-02-07 20:50:28 +0900

    Login page, app stack added

---
+++ client/appstack/src/core/theme.js
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    surface: '#cde5fa',
+    text: '#000000',
+    primary: '#237ed9',
+    secondary: '#414757',
+    error: '#f13a59',
+
+  },
+}
